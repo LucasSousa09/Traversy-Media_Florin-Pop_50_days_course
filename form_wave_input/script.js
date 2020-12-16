f26b8167715cf4551f98d580a6c490f2d5da6cb8
Lucas <lucasrodrigues.sousa09@gmail.com>
2020-12-16 15:03:00 -0300

    Two new projects
    I added Form Wave and Sound Boards.

---
+++ form_wave_input/script.js
@@ -0,0 +1,20 @@
+const labels = document.querySelectorAll(".form-control label")
+const input = document.querySelector(".email")
+const form = document.querySelector(".form-control")
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split("")
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join("")
+})
+
+
+input.addEventListener("keyup", (evt)=> {
+    if(input.value !== ""){
+        form.classList.add("active")
+    }
+    else{
+        form.classList.remove("active")
+    }
+})
