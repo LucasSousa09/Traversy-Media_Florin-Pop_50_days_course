b06afb3279daaa88d77bcf67dc07a3f1cce9484d
Lucas <lucasrodrigues.sousa09@gmail.com>
2020-12-23 11:10:57 -0300

    Fixed spin issue on clock
    The spin is a preference question, but I preferred to take it out.

--- theme_clock/script.js
+++ theme_clock/script.js
@@ -4,7 +4,9 @@ const secondEl = document.querySelector(".second")
 const timeEl = document.querySelector(".time")
 const dateEl = document.querySelector(".date")
 const toggle = document.querySelector(".toggle")
-
+const needleSecond = document.querySelector(".needle.second")
+const needleMinute = document.querySelector(".needle.minute")
+const needleHour = document.querySelector(".needle.hour") 
 const days = ["Segunda-Feira","Terça-Feira","Quarta-Feira","Quinta-Feira","Sexta-Feira","Sábado","Domingo"]
 const months = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
 
@@ -32,8 +34,13 @@ function setTime() {
     const ampm = hours >= 12 ? "PM" : "AM"
 
     hourEl.style.transform = `translate(-50%,-100%) rotate(${scale(hourForClock, 0, 11, 0, 360)}deg)`
+    needleHour.style.transition = `${hours === 0 ? "none" : "all 0.5s ease-in"}`
+
     minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes,0,59,0,360)}deg)`
+    needleMinute.style.transition = `${minutes === 0 ? "none" : "all 0.5s ease-in"}`
+
     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+    needleSecond.style.transition = `${seconds === 0 ? "none" : "all 0.5s ease-in"}`
 
     timeEl.innerHTML = `${hourForClock}:${minutes < 10 ? "0"+ minutes : minutes}${ampm}`
     dateEl.innerHTML = `<span class="circle">${date}</span> de ${months[month]}, ${days[day]}`
